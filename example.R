2fa7a25158598d00fcf28db89e50a4f1cc137873
Joe Brew <joebrew@gmail.com>
2016-06-28 13:32:49 +0200

    done with first pass

--- example.R
+++ example.R
@@ -8,7 +8,7 @@ source('get_weather.R')
 
 # Get weather
 x = get_weather(station = "MPM", 
-                start_year = 2014,
+                start_year = 2010,
                 end_year = 2016)
 
 # Make data long instead of wide
@@ -25,3 +25,13 @@ ggplot(data = long %>%
   geom_point(alpha = 0.2) +
   geom_smooth() +
   theme_fivethirtyeight()
+
+# put all lines on one year
+x$day <-as.numeric(format(x$date, "%j"))
+x$mon <-as.numeric(format(x$date, "%m"))
+x$year <-as.numeric(format(x$date, "%Y"))
+
+# Get average precipitation by year month
+y <- x %>%
+  group_by(year, mon) %>%
+  summarize(mprec=mean(precipitation))
